5d58b6885ac33406847d82bb57a2dd8cebfa7d3c
iamnk <offl.naveenkumar@gmail.com>
2018-10-10 01:42:52 +0530

    Created Model for IBTSapp, 1st commit

---
+++ IBTS_site/card_validation/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    name=models.CharField(max_length=200)
+    roll_no=models.CharField(max_length=10)
+    Fee = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.roll_no
+
+class StudentEntry(models.Model):
+    student=models.ForeignKey(Student, on_delete=models.CASCADE)
+    date=models.DateField()
