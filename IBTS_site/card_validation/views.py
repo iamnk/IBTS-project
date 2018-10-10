da6df99fc509ed02f0413068992d2051260cdd23
iamnk <offl.naveenkumar@gmail.com>
2018-10-10 11:51:21 +0530

    Check modified

--- IBTS_site/card_validation/views.py
+++ IBTS_site/card_validation/views.py
@@ -13,7 +13,7 @@ def validation(request, rf_id):
         return HttpResponse("WELCOME %s"%studentobj.roll_no)
     else:
         travel_count=11-studentobj.studententry_set.count()
-        if(travel_count<12):
+        if(travel_count>0):
             tdydate=datetime.date.today()
             entry=StudentEntry.objects.create(student=studentobj,date=tdydate)
             entry.save()
